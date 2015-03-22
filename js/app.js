e62811256cc81e6564b5733b3151f7248cb3e8ae
Charuwit Nodthaisong <tui2tone@gmail.com>
2015-03-23 02:11:39 +0700

    Initial Menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-(function() {
-
-
-}).call(this);
+$(function () {
+    $('.navbar-toggle').click(function () {
+        $('.navbar-nav').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+        $('#search').removeClass('in').addClass('collapse').slideUp(200);
+    });
+   
+   $('.navbar-expand-toggle').click(function () {
+        $('.navbar-expand-toggle').toggleClass('fa-rotate-90');
+        $('.content-container').toggleClass('expanded');
+    });
+});
