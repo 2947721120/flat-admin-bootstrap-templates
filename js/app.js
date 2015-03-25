114857e3e6f52baad7ae4c41e8cbddf2df7e15ad
Charuwit Nodthaisong <tui2tone@gmail.com>
2015-03-26 00:44:56 +0700

    Add Noti and Card Pages

--- js/app.js
+++ js/app.js
@@ -1,43 +1,80 @@
 (function() {
   $(function() {
-    var ctx, data, legend, myLineChart, options, options2;
+    return $(".navbar-expand-toggle").click(function() {
+      $(".content-container").toggleClass("expanded");
+      return $(".navbar-expand-toggle").toggleClass("fa-rotate-90");
+    });
+  });
+
+  $(function() {
+    var ctx, ctx_light, data, data_light, myLineChart, myLineChartLight, options, options_light;
     Chart.defaults.global.responsive = true;
     ctx = $('#dashboard-order-chart').get(0).getContext('2d');
+    ctx_light = $('#dashboard-order-chart-light').get(0).getContext('2d');
     options = {
       bezierCurve: false,
-      pointDotRadius: 8,
+      pointDotRadius: 0,
       datasetStroke: true,
-      datasetStrokeWidth: 2,
-      scaleGridLineColor: 'rgba(0,0,0,.05)',
-      scaleFontColor: '#666',
+      datasetStrokeWidth: 5,
+      scaleGridLineColor: 'rgba(255,255,255,0)',
+      scaleFontColor: 'rgba(255,255,255,.4)',
+      scaleLineColor: 'rgba(0,0,0,0)',
+      scaleShowVerticalLines: false,
+      scaleShowGridLines: false,
+      scaleOverride: false,
+      scaleShowLabels: false,
+      scaleSteps: 10,
+      pointDotStrokeWidth: 0,
+      showScale: true
+    };
+    options_light = {
+      bezierCurve: false,
+      pointDotRadius: 0,
+      datasetStroke: true,
+      datasetStrokeWidth: 5,
+      scaleGridLineColor: 'rgba(255,255,255,0)',
+      scaleFontColor: 'rgba(0,0,0,.4)',
       scaleLineColor: 'rgba(0,0,0,0)',
       scaleShowVerticalLines: false,
       scaleShowGridLines: true,
       scaleOverride: false,
       scaleSteps: 10,
-      pointDotStrokeWidth: 2
-    };
-    options2 = {
-      percentageInnerCutout: 75,
-      legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+      scaleShowLabels: false,
+      pointDotStrokeWidth: 0,
+      showScale: true
     };
     data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September'],
       datasets: [
         {
           label: 'My First dataset',
-          fillColor: 'rgba(0,177,106,0.3)',
-          strokeColor: 'rgba(0,177,106,1)',
+          fillColor: 'transparent',
+          strokeColor: '#FFF',
           pointColor: '#FFF',
           pointStrokeColor: 'rgba(0,177,106,1)',
           pointHighlightFill: '#fff',
           pointHighlightStroke: 'rgba(220,220,220,1)',
-          data: [65, 59, 80, 81, 56, 55, 40]
+          data: [35000, 38000, 29000, 40000, 34500, 41000, 42000, 56000, 80000]
+        }
+      ]
+    };
+    data_light = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          fillColor: 'transparent',
+          strokeColor: '#22A7F0',
+          pointColor: '#FFF',
+          pointStrokeColor: 'rgba(0,177,106,1)',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data: [39, 32, 42, 40, 58, 66, 78, 80, 99]
         }
       ]
     };
     myLineChart = new Chart(ctx).Line(data, options);
-    legend = myBarChart.generateLegend();
+    myLineChartLight = new Chart(ctx_light).Line(data_light, options_light);
   });
 
 }).call(this);
--- js/coffeescript/app.coffee
+++ js/coffeescript/app.coffee
@@ -1,23 +1,45 @@
+# Navbar
+$ ->
+  $(".navbar-expand-toggle").click ->
+    $(".content-container").toggleClass "expanded"
+    $(".navbar-expand-toggle").toggleClass "fa-rotate-90"
+
 # Chart.JS
 $ ->
   Chart.defaults.global.responsive = true
   ctx = $('#dashboard-order-chart').get(0).getContext('2d')
+  ctx_light = $('#dashboard-order-chart-light').get(0).getContext('2d')
   options = 
     bezierCurve: false
-    pointDotRadius: 8
+    pointDotRadius: 0
     datasetStroke: true
-    datasetStrokeWidth: 2
-    scaleGridLineColor: 'rgba(0,0,0,.05)'
-    scaleFontColor: '#666'
+    datasetStrokeWidth: 5
+    scaleGridLineColor: 'rgba(255,255,255,0)'
+    scaleFontColor: 'rgba(255,255,255,.4)'
+    scaleLineColor: 'rgba(0,0,0,0)'
+    scaleShowVerticalLines: false
+    scaleShowGridLines: false
+    scaleOverride: false
+    scaleShowLabels: false
+    scaleSteps: 10
+    pointDotStrokeWidth: 0
+    showScale: true
+
+  options_light = 
+    bezierCurve: false
+    pointDotRadius: 0
+    datasetStroke: true
+    datasetStrokeWidth: 5
+    scaleGridLineColor: 'rgba(255,255,255,0)'
+    scaleFontColor: 'rgba(0,0,0,.4)'
     scaleLineColor: 'rgba(0,0,0,0)'
     scaleShowVerticalLines: false
     scaleShowGridLines: true
     scaleOverride: false
     scaleSteps: 10
-    pointDotStrokeWidth: 2
-  options2 = 
-    percentageInnerCutout: 75
-    legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+    scaleShowLabels: false
+    pointDotStrokeWidth: 0
+    showScale: true
   data = 
     labels: [
       'January'
@@ -27,27 +49,64 @@ $ ->
       'May'
       'June'
       'July'
+      'August'
+      'September'
     ]
     datasets: [ {
       label: 'My First dataset'
-      fillColor: 'rgba(0,177,106,0.3)'
-      strokeColor: 'rgba(0,177,106,1)'
+      fillColor: 'transparent'
+      strokeColor: '#FFF'
       pointColor: '#FFF'
       pointStrokeColor: 'rgba(0,177,106,1)'
       pointHighlightFill: '#fff'
       pointHighlightStroke: 'rgba(220,220,220,1)'
       data: [
-        65
-        59
-        80
-        81
-        56
-        55
+        35000
+        38000
+        29000
+        40000
+        34500
+        41000
+        42000
+        56000
+        80000
+      ]
+    } ]
+
+  data_light = 
+    labels: [
+      'January'
+      'February'
+      'March'
+      'April'
+      'May'
+      'June'
+      'July'
+      'August'
+      'September'
+    ]
+    datasets: [ {
+      label: 'My First dataset'
+      fillColor: 'transparent'
+      strokeColor: '#22A7F0'
+      pointColor: '#FFF'
+      pointStrokeColor: 'rgba(0,177,106,1)'
+      pointHighlightFill: '#fff'
+      pointHighlightStroke: 'rgba(220,220,220,1)'
+      data: [
+        39
+        32
+        42
         40
+        58
+        66
+        78
+        80
+        99
       ]
     } ]
   myLineChart = new Chart(ctx).Line(data, options)
-  legend = myBarChart.generateLegend()
+  myLineChartLight = new Chart(ctx_light).Line(data_light, options_light)
   return
 
 # ---
