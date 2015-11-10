059ee516c14b3fa718c0c8c2621eaf3d6f18d516
tui2tone <tui2tone@gmail.com>
2015-11-11 01:01:30 +0700

    remove bower_component, add bower.json package.json and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp"),
+    coffee = require("gulp-coffee"),
+    sass = require("gulp-sass");
+
+gulp.task("coffee", function() {
+
+});
+
+gulp.task("sass", function() {
+
+});
+
+gulp.task("watch", function() {
+
+});
