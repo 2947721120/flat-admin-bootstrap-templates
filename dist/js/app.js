a449e7ee35a7eecb0c532eef29bc0a9cf692c316
tui2tone <tui2tone@gmail.com>
2015-12-05 00:48:23 +0700

    Create Dist

---
+++ dist/js/app.js
@@ -0,0 +1,35 @@
+$(function() {
+  $(".navbar-expand-toggle").click(function() {
+    $(".app-container").toggleClass("expanded");
+    return $(".navbar-expand-toggle").toggleClass("fa-rotate-90");
+  });
+  return $(".navbar-right-expand-toggle").click(function() {
+    $(".navbar-right").toggleClass("expanded");
+    return $(".navbar-right-expand-toggle").toggleClass("fa-rotate-90");
+  });
+});
+
+$(function() {
+  $('.input-checkbox, .input-radio').iCheck({
+    checkboxClass: 'icheckbox_flat',
+    radioClass: 'iradio_flat'
+  });
+});
+
+$(function() {
+  return $('select').select2();
+});
+
+$(function() {
+  return $('.toggle-checkbox').bootstrapSwitch();
+});
+
+$(function() {
+  return $('.match-height').matchHeight();
+});
+
+$(function() {
+  return $('.datatable').DataTable({
+    "dom": '<"top"fl<"clear">>rt<"bottom"ip<"clear">>'
+  });
+});
